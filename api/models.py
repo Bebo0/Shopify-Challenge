7adfd70a7d5ba4b96f778acdd73474fd5372ca56
Bebo Elhosary <beboelhosary@hotmail.com>
2018-09-21 21:43:33 -0400

    Add models

--- api/models.py
+++ api/models.py
@@ -1,3 +1,42 @@
 from django.db import models
 
-# Create your models here.
+class Shop(models.Model):
+	products = models.ManyToManyField(Product)
+	orders = models.ManyToManyField(Order)
+
+
+	def __str__(self):
+		return self.title
+
+	def body_preview(self):
+		return self.body[:50]
+
+class Product(models.Model):
+	line_items = models.ManyToManyField(LineItem)
+	value = models.FloatField()
+
+	def __str__(self):
+		return self.title
+
+	def body_preview(self):
+		return self.body[:50]
+
+class LineItem(models.Model):
+	product = models.ForeignKey(Product)
+	value = models.FloatField()
+
+	def __str__(self):
+		return self.title
+
+	def body_preview(self):
+		return self.body[:50]
+
+class Order(models.Model):
+	line_items = models.ManyToManyField(LineItem)
+	value = models.FloatField()
+
+	def __str__(self):
+		return self.title
+
+	def body_preview(self):
+		return self.body[:50]
